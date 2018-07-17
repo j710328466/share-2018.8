51f2b770a419ecaa823792f8b3afdcf783bf83aa
江志雄 <jiangzhixiong@jimistore.com>
2018-07-17 15:06:01 +0800

    测试

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use('/md5', (req, res, next) => {
   if (val) {
     res.send(utility.md5(val))
   } else {
-    res.send('你可以在链接传个值看看啊~（q=XXX）!')
+    res.send('你可以在链接传个值看看啊 （q=XXX）!')
   }
 })
 
